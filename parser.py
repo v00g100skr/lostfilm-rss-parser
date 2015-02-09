bfbec2697108d613811c621f72917cd61e3f2e6c
Artem Voronkin <a.voronkin@wysegames.com>
2015-02-09 18:25:41 +0200

    parse download.log to prevent dublicate downloading

--- parser.py
+++ parser.py
@@ -21,12 +21,22 @@ def main():
     log_error = logging.getLogger('error')
     log_download = logging.getLogger('download')
 
+    downloaded_torrents = []
+
+    file=open("download.log",'r')
+    row = file.readlines()
+    for line in row:
+        items = line.split(" ")
+        downloaded_torrents.append(items[1].replace("\n",''))
+    file.close()
+
     try:
         tc = transmissionrpc.Client(
             address=settings.TRANSMISSION_HOST,
             port=settings.TRANSMISSION_PORT,
             user=settings.TRANSMISSION_USER,
             password=settings.TRANSMISSION_PASS)
+        log_process.info('connected to transmission ({}:{})'.format(settings.TRANSMISSION_HOST,settings.TRANSMISSION_PORT))
     except TransmissionError as e:
         log_error.error('{} : {}'.format('TransmissionError',e.message))
         tc = False
@@ -45,6 +55,9 @@ def main():
             download_dir = settings.TORRENTS_PATH + download_path
 
             if all([regexp in entry.title, quality in entry.link]):
+                if torrent_filename in downloaded_torrents:
+                    log_process.info('{} already in transmission - skipping'.format(torrent_filename))
+                    continue
                 log_process.info('{} matched'.format(torrent_filename))
                 request = urllib2.Request(
                     entry.link,
@@ -65,6 +78,8 @@ def main():
                         tc.add_torrent(base64.b64encode(buffer), download_dir=download_dir)
                         log_download.info(torrent_filename)
                         log_process.info('{} added to transmission'.format(torrent_filename))
+                    else:
+                        log_error.error('no connection  to transmission - skipping')
                 else:
                     log_process.warning('{} has zero size'.format(torrent_filename))
 
